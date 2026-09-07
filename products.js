8d0e0c649fd01c94a28ca43f730a8974053ef9bb
mareajewelshop-cmd <mareajewel.shop@gmail.com>
2026-09-07 20:31:46 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 const products = [
   {id:1,name:"Producto 1",category:"Collares",price:"39,90 €",description:"Una silueta inspirada en el vuelo de una mantarraya.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"},
-  {id:2,name:"Producto 2",category:"Pendientes",price:"29,90 €",description:"Formas orgánicas inspiradas en la vida de la costa.",image:"https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?auto=format&fit=crop&w=1000&q=85"},
-  {id:3,name:"Producto 3",category:"Pulseras",price:"24,90 €",description:"Un diseño sencillo inspirado en el movimiento del agua.",image:"https://images.unsplash.com/photo-1573408301185-9146fe634ad0?auto=format&fit=crop&w=1000&q=85"},
-  {id:4,name:"Producto 4",category:"Anillos",price:"27,90 €",description:"Una pieza delicada con una forma inspirada en la arena.",image:"https://images.unsplash.com/photo-1605100804763-247f67b3557e?auto=format&fit=crop&w=1000&q=85"},
-  {id:5,name:"Producto 5",category:"Collares",price:"34,90 €",description:"Una pieza minimalista con espíritu de océano abierto.",image:"https://images.unsplash.com/photo-1617038220319-276d3cfab638?auto=format&fit=crop&w=1000&q=85"},
-  {id:6,name:"Producto 6",category:"Pulseras",price:"22,90 €",description:"Una línea suave que recuerda al movimiento de una ola.",image:"https://images.unsplash.com/photo-1611652022419-a9419f74343d?auto=format&fit=crop&w=1000&q=85"}
+  {id:2,name:"Producto 2",category:"Pendientes",price:"29,90 €",description:"Formas orgánicas inspiradas en la vida de la costa.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"},
+  {id:3,name:"Producto 3",category:"Pulseras",price:"24,90 €",description:"Un diseño sencillo inspirado en el movimiento del agua.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"},
+  {id:4,name:"Producto 4",category:"Anillos",price:"27,90 €",description:"Una pieza delicada con una forma inspirada en la arena.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"},
+  {id:5,name:"Producto 5",category:"Collares",price:"34,90 €",description:"Una pieza minimalista con espíritu de océano abierto.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"},
+  {id:6,name:"Producto 6",category:"Pulseras",price:"22,90 €",description:"Una línea suave que recuerda al movimiento de una ola.",image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?auto=format&fit=crop&w=1000&q=85"}
 ];
